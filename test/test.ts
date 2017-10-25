82da4fe1d980e89793c34989b11a50caaf321bc7
jjrv <juha@busfaster.com>
2017-10-25 10:18:33 +0300

    Improve tests.

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-import { types, onSnapshot } from 'mobx-state-tree';
+import { types, process, onSnapshot } from 'mobx-state-tree';
 import { mst, shim, action } from '..';
 
 const TodoData = shim(types.model({
@@ -22,13 +22,18 @@ class TodoCode extends TodoData {
 
 const Todo = mst(TodoCode, TodoData);
 
-const SpecialTodoData = shim(Todo);
+const SpecialTodoData = shim(
+	Todo.props({
+		count: types.optional(types.number, 0)
+	}),
+	Todo
+);
 
 class SpecialTodoCode extends SpecialTodoData {
 
 	@action
 	toggle() {
-		console.log('Toggling!');
+		console.log('Toggled ' + this.title + ' ' + (++this.count) + ' times!');
 		super.toggle();
 	}
 
@@ -40,14 +45,42 @@ const Store = types.model({
 	todos: types.array(SpecialTodo)
 });
 
-const store = Store.create({ todos: [{
-	title: "Get coffee"
-}]});
+const store = Store.create({
+	todos: [
+		{ title: 'Foo' },
+		{ title: 'Bar' }
+	]
+});
 
 onSnapshot(store, (snapshot) => {
 	console.log(snapshot)
 })
 
-store.todos[0].print()
-store.todos[0].toggle()
-store.todos[0].print()
+store.todos[0].toggle();
+store.todos[1].toggle();
+store.todos[1].toggle();
+
+store.todos[0].print();
+store.todos[1].print();
+
+const AsyncData = shim(types.model({}));
+
+class AsyncCode extends AsyncData {
+
+	@action
+	run() {
+		function* generate() {
+			yield Promise.resolve('This gets lost');
+			return('Returned value');
+		}
+
+		return(process(generate)());
+	}
+
+}
+
+const Async = mst(AsyncCode, AsyncData);
+
+Async.create().run().then(
+	(result) => console.log(result)
+);
