8689fdf346175a5664dd10611f1f53c182342800
jjrv <juha@busfaster.com>
2017-12-07 20:58:56 +0200

    Update dependencies, improve build script and bump version.

--- src/fix-amd.js
+++ src/fix-amd.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
 
+var name = process.argv[2];
 var codePath = require('path').resolve(__dirname, '../dist/index-amd.js');
 var code = fs.readFileSync(codePath, { encoding: 'utf-8' });
 
-code = code.replace(/\"src\//g, '"classy-mst/');
+code = code.replace(/\"src\//g, '"' + name + '/');
 
 fs.writeFileSync(codePath, code, { encoding: 'utf-8' });
